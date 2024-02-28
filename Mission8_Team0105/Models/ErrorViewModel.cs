1a760957b28425b2280bfa0d640a6de64899a6f4
sydneygroverr <sydney.grover8@gmail.com>
2024-02-28 10:33:15 -0700

    Add project files.

---
+++ Mission8_Team0105/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission8_Team0105.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
