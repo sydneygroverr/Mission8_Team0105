a1715de41c9d769b4bb06a5d3ab0e2b2fdb29e04
sydneygroverr <sydney.grover8@gmail.com>
2024-03-01 12:53:49 -0700

    fixing changes

--- Mission8_Team0105/Controllers/HomeController.cs
+++ Mission8_Team0105/Controllers/HomeController.cs
@@ -44,86 +44,7 @@ using System.Runtime.CompilerServices;
 //        public IActionResult AddTask()
 //        {
 //            ViewBag.Tasks = _context.Tasks
-//                .OrderBy(x => x.TaskName)
-//                .ToList();
-
-//            return View("AddTask");
-//        }
-
-//        [HttpPost]
-//        public IActionResult AddTask(Movie response)
-//        {
-
-//            // add the record to the database
-//            _context.Movies.Add(response);
-//            _context.SaveChanges(); //commit changes to the database
-
-//            return View("Confirmation", response);
-//        }
-
-//        public IActionResult TaskData()
-//        {
-//            var collections = _context.Movies.Include("Category")
-//                .OrderBy(x => x.Title).ToList();
-
-//            return View(collections);
-//        }
-
-
-//        [HttpGet]
-//        public IActionResult Edit(int id)
-//        {
-//            var recordToEdit = _context.Movies
-//                .Single(x => x.MovieId == id);
-
-//            ViewBag.Categories = _context.Categories
-//            .OrderBy(x => x.CategoryName)
-//            .ToList();
-
-//            return View("AddMovie", recordToEdit);
-//        }
-
-//        [HttpPost]
-//        public IActionResult Edit(Movie updatedInfo)
-//        {
-//            _context.Update(updatedInfo);
-//            _context.SaveChanges();
-
-//            return RedirectToAction("MovieData");
-//        }
-
-//        [HttpGet]
-//        public IActionResult Delete(int id)
-//        {
-//            var recordToDelete = _context.Movies
-//                .Single(x => x.MovieId == id);
-
-//            return View(recordToDelete);
-//        }
-
-//        [HttpPost]
-//        public IActionResult Delete(Movie movie)
-//        {
-//            _context.Movies.Remove(movie);
-//            _context.SaveChanges();
-
-//            return RedirectToAction("MovieData");
-//        }
-//    }
-//}
-
-
-
-
-
-
-
-
-
-
-// mission 7 controller
-
-
+//              
 
 namespace Mission8_Team0105.Controllers
 {
@@ -181,8 +102,8 @@ namespace Mission8_Team0105.Controllers
         {
 
             // need to change the variable we are calling on here
-            var collections = _context.Tasks.Include("Category")
-                .OrderBy(x => x.Title).ToList();
+            var collections = _context.Tasks.Include("Category");
+                //.OrderBy(x => x.CategoryName).ToList();
 
             // need to change the return view here
             return View(collections);
